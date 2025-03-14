ee1f2d58ffe0748cd1ca40e65b22d61e6ebd6c1b
Carlos Henrique <carlos_henrique.taqua@hotmail.com>
2025-03-13 22:51:33 -0300

    Ajustes de layout
    - Criado componente de input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
 import { Button } from "@/components/button";
+import { IconButton } from '@/components/icon-button';
+import { Input, InputField, InputIcon, InputRoot } from '@/components/input';
 
 export default function Home() {
   return (
     <main>
       <Button>
-        Enviar
+        Uau
         <ArrowRight />
       </Button>
+      <IconButton>
+        <Copy />
+      </IconButton>
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className='size-5'/>
+          </InputIcon>
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
